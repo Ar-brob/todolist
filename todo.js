9a997526d2897216893d7ab7b0e1afbe7fab7bd8
temkawerka@gmail.com <temkawerka@gmail.com>
2025-11-24 18:34:56 +0500

    seventh ot eight

--- todo.js
+++ todo.js
@@ -1,57 +1,3 @@
-const wind = document.getElementById('wind')
-const temo = document.getElementById('temo')
-const open = document.getElementById('open')
-const bright = document.getElementsByClassName('bright')
-const dark = document.getElementsByClassName('darki')
-const white = document.getElementsByClassName('white')
-const nit = document.getElementById('nit')
-const closer = document.getElementById('closer')
-const sun = document.getElementsByClassName('sun')
-const darknit = document.getElementsByClassName('darknit')
-const lightnit = document.getElementsByClassName('lightnit ')
-const closedark = document.getElementsByClassName('closedark')
-const whiteclose = document.getElementsByClassName('whiteclose')
-const poisk2dark = document.getElementsByClassName('poisk2dark')
-const klickday = document.getElementsByClassName('klick-day')
-const klicknight = document.getElementsByClassName('klick-night')
-const all = document.getElementById('all')
-const not_all = document.getElementById('not_all')
-const ended = document.getElementById('ended')
-document.getElementById('klick') .addEventListener ('click', (event) => {
-    event.preventDefault();
-    const tip = document.body.className;
-    if (tip === 'bright' ) {
-        document.body.classList = 'darkt'
-        wind.classList = 'darki'
-        nit.classList = 'darknit'
-        closer.classList = 'closedark'
-       klick.classList = 'klick-night'
-
-
-
-     
-     
-
-    } else {
-            document.body.classList = 'bright'
-             wind.classList = 'white'
-             closer.classList = 'whiteclose'
-             nit.classList = 'lightnit'
-              klick.classList = 'klick-day'
-            
-    }
-})
-document.getElementById ('open').addEventListener ('click', (iviv) =>{
-    iviv.preventDefault()
-    wind.style.display  = 'flex'
-    temo.style.display = 'block'
-}) 
-document.getElementById ('closer').addEventListener ('click', (ivive) =>{
-    ivive.preventDefault()
-    wind.style.display  = 'none'
-    temo.style.display = 'none'
-})
-
 
 const notes = document.getElementById('poisk2');
 const note_list = document.getElementById('note_list');
@@ -152,3 +98,66 @@ note_list.addEventListener('mouseover', () => {
         });
     } 
 });
+const wind = document.getElementById('wind')
+const temo = document.getElementById('temo')
+const open = document.getElementById('open')
+const bright = document.getElementsByClassName('bright')
+const dark = document.getElementsByClassName('darki')
+const white = document.getElementsByClassName('white')
+const nit = document.getElementById('nit')
+const closer = document.getElementById('closer')
+const sun = document.getElementsByClassName('sun')
+const darknit = document.getElementsByClassName('darknit')
+const lightnit = document.getElementsByClassName('lightnit ')
+const closedark = document.getElementsByClassName('closedark')
+const whiteclose = document.getElementsByClassName('whiteclose')
+const poisk2dark = document.getElementsByClassName('poisk2dark')
+const klickday = document.getElementsByClassName('klick-day')
+const klicknight = document.getElementsByClassName('klick-night')
+const all = document.getElementById('all')
+const not_all = document.getElementById('not_all')
+const ended = document.getElementById('ended')
+const wind2 =   document.getElementById('wind2')
+const whiter = document.getElementsByClassName('whiter')
+const nighter = document.getElementsByClassName('nighter')
+const net = document.getElementById('net')
+const darknet = document.getElementsByClassName('darknet')
+const lightnet = document.getElementsByClassName('lightnet ')
+const closesar = document.getElementById('closesar')
+const closesardark = document.getElementsByClassName('closesardark')
+const closesarwhite = document.getElementsByClassName('closesarwhite')
+document.getElementById('klick') .addEventListener ('click', (event) => {
+    event.preventDefault();
+    const tip = document.body.className;
+    if (tip === 'bright' ) {
+        document.body.classList = 'darkt'
+        wind.classList = 'darki'
+        nit.classList = 'darknit'
+        closer.classList = 'closedark'
+       klick.classList = 'klick-night'
+       wind2.classList = 'nighter'
+       net.classList = 'darknet'
+       closesar.classList = 'closesardark'
+ } else {
+            document.body.classList = 'bright'
+             wind.classList = 'white'
+             closer.classList = 'whiteclose'
+             nit.classList = 'lightnit'
+              klick.classList = 'klick-day'
+              wind2.classList = 'whiter'
+              net.classList = 'lightnet'
+              closesar.classList = 'closesarwhite'
+            
+    }
+})
+document.getElementById ('open').addEventListener ('click', (iviv) =>{
+    iviv.preventDefault()
+    wind.style.display  = 'flex'
+    temo.style.display = 'block'
+}) 
+document.getElementById ('closer').addEventListener ('click', (ivive) =>{
+    ivive.preventDefault()
+    wind.style.display  = 'none'
+    temo.style.display = 'none'
+})
+
